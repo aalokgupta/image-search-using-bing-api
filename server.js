b0714c40b27b1c91005db915aba8c55696beb9c9
System <none>
2017-09-24 14:44:09 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 var Bing = require('node-bing-api')({"accKey": 'x1kE54sILOhbBK8+HS3uHc010M+A1euytKKbKkuTzN0'});
 var util = require('util');
 var searchBing = util.promisify(Bing.web.bind(Bing));
+var url = require('url');
 
 //var search = new Search('x1kE54sILOhbBK8+HS3uHc010M+A1euytKKbKkuTzN0');
 // we've started you off with Express, 
@@ -21,10 +22,11 @@ app.get("/", function (request, response) {
 });
 
 app.get("/api/imagesearch/:image_type", function (request, response) {
-  
   var query = request.params["image_type"];
   var count = request.params["offset"]
-  response.json({"image type ": query});
+  var body  = url.parse(request.body);
+  request.send(body);
+  // response.json({"image type ": query, "Url": "abcd"});
   // Bing.images(query, {count: 10}, function(err, result){
   //   response.send(result);
   // });
